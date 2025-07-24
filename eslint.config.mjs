89fac10359ac13089be44d3c4d3cc6a9585266a3
NDOUR <adama.ndour@cimmyt.org>
2025-07-24 16:43:12 +0300

    disabling eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,17 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  // 1) pull in Next’s defaults:
+  ...compat.extends("next/core-web-vitals"),
+
+  // 2) then override/add your own rules:
+  {
+    rules: {
+      // disable the “no unescaped entities” check in JSX:
+      "react/no-unescaped-entities": "off",
+    },
+  },
+];
 
 export default eslintConfig;
